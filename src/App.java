ce519a4fe51374df18f0a2bd36c534064605f60a
yoanasonia <yoanasoniaa@gmail.com>
2022-04-16 13:56:37 +0700

    overview

--- src/App.java
+++ src/App.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,7 +9,7 @@ public class App extends Application {
 
     public void start(Stage stage) throws IOException {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/firstPage.fxml"));
+            loader.setLocation(getClass().getResource("/userCheckout.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root);
             stage.setScene(scene);
